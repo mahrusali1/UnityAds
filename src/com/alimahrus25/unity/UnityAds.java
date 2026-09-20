d853592f5b8bb5a4b6d1d6cf0ee3162c2660a691
mahrusali1 <126897165+mahrusali1@users.noreply.github.com>
2026-09-20 09:43:58 +0700

    Update UnityAds.java

--- src/com/alimahrus25/unity/UnityAds.java
+++ src/com/alimahrus25/unity/UnityAds.java
@@ -1,6 +1,9 @@
 package com.alimahrus25.unity;
 
+import android.widget.FrameLayout;
+
 import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
@@ -12,7 +15,6 @@ import com.unity3d.services.banners.BannerView;
 import com.unity3d.services.banners.BannerErrorInfo;
 import com.unity3d.services.banners.UnityBannerSize;
 
-
 @DesignerComponent(
         version = 1,
         description = "Unity Ads Extension Test",
@@ -23,7 +25,15 @@ import com.unity3d.services.banners.UnityBannerSize;
 @SimpleObject(external = true)
 public class UnityAds extends AndroidNonvisibleComponent {
 
+    private BannerView bannerView;
+    private FrameLayout bannerContainer;
+
     public UnityAds(ComponentContainer container) {
         super(container.$form());
+    }
+
+    @SimpleFunction(description = "Test Unity Ads SDK")
+    public void TestSDK() {
+        boolean ready = com.unity3d.ads.UnityAds.isInitialized();
     }
 }
