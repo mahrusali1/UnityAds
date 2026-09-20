0c4cabfebc71b8a3509195ea1799a141341d017d
mahrusali1 <126897165+mahrusali1@users.noreply.github.com>
2026-09-20 08:32:44 +0700

    Update UnityAds.java

--- src/com/alimahrus25/unity/UnityAds.java
+++ src/com/alimahrus25/unity/UnityAds.java
@@ -17,12 +17,6 @@ import com.google.appinventor.components.runtime.ComponentContainer;
 import com.google.appinventor.components.runtime.EventDispatcher;
 import com.google.appinventor.components.runtime.Form;
 
-import com.unity3d.ads.IUnityAdsInitializationListener;
-import com.unity3d.ads.UnityAds.UnityAdsInitializationError;
-
-import com.unity3d.services.banners.BannerErrorInfo;
-import com.unity3d.services.banners.BannerView;
-import com.unity3d.services.banners.UnityBannerSize;
 
 
 @DesignerComponent(
@@ -50,9 +44,7 @@ public class UnityAds extends AndroidNonvisibleComponent {
     private boolean initialized = false;
     private boolean bannerLoaded = false;
 
-    private BannerView bannerView;
-    private FrameLayout bannerContainer;
-
+  
 
           public UnityAds(ComponentContainer container) {
     super(container.$form());
